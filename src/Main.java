c8fb8ee0c2c001a38f0fbd1215b3b9c1a4c3a9dc
sachin lobo <lobosachins@gmail.com>
2022-11-08 21:23:39 +0530

    Added Empty class

--- src/Main.java
+++ src/Main.java
@@ -11,25 +11,26 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-
 public class Main {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 
 
         FileInputStream serviceAccount = null;
         try {
-
-
             serviceAccount = new FileInputStream("numbergame-de078-firebase-adminsdk-whekw-084c44b6c4.json");
         } catch (Exception e) {
         }
 
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
 
-            FirebaseApp.initializeApp(options);
-            System.out.println("setup done");
+        FirebaseApp.initializeApp(options);
+        System.out.println("setup done");
+        serviceForsend();
+    }
+
+            public static void serviceForsend() throws ExecutionException, InterruptedException {
         Firestore db = FirestoreClient.getFirestore();
 
         Map<String, Object> data = new HashMap<>();
@@ -37,7 +38,10 @@ public class Main {
         data.put("lastname", "lobo");
         data.put("points",1000);
         ApiFuture<WriteResult> result = db.collection("users").document("Number").set(data);
+
         System.out.println("Update time : " + result.get().getUpdateTime());
+
+
     }
         }
 
