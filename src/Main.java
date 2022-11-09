8993c966f06ae5605036af8b575db62f9117060f
sachin lobo <lobosachins@gmail.com>
2022-11-09 18:34:45 +0530

    Refractored the code

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
-import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 public class Main {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-
 
         FileInputStream serviceAccount = null;
         try {
             serviceAccount = new FileInputStream("numbergame-de078-firebase-adminsdk-whekw-084c44b6c4.json");
         } catch (Exception e) {
+            System.out.println("Public key File couldnt read! ");
         }
 
         FirebaseOptions options = new FirebaseOptions.Builder()
@@ -27,23 +22,18 @@ public class Main {
 
         FirebaseApp.initializeApp(options);
         System.out.println("setup done");
-        serviceForsend();
+      ///**************************
+
+
+        System.out.println("Host/ Player?");
+Scanner sc =new Scanner(System.in);
+String Opinion = sc.nextLine();
+
+        FirebaseService.serviceForsend(Opinion);
+        //FirebaseService.serviceForRead();
     }
 
-            public static void serviceForsend() throws ExecutionException, InterruptedException {
-        Firestore db = FirestoreClient.getFirestore();
-
-        Map<String, Object> data = new HashMap<>();
-        data.put("name", "sachin");
-        data.put("lastname", "lobo");
-        data.put("points",1000);
-        ApiFuture<WriteResult> result = db.collection("users").document("Number").set(data);
-
-        System.out.println("Update time : " + result.get().getUpdateTime());
-
-
-    }
-        }
+}
 
 
 
